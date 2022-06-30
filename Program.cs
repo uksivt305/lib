f0bdb549bb10b59805336ebfb228a0d3a61a3535
Timur <nur.timur@ufaga.ru>
2022-06-30 19:28:09 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Clib;
+namespace ConsoleAppClib
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClib tl= new TestClib();
+            Console.WriteLine("Введите строку:");
+            string d = Console.ReadLine();
+            Console.WriteLine($"Удвоенная строка:{  tl.Double(d)}");
+            Console.WriteLine($"Количество гласных:{tl.Glas(d)}");
+            Console.WriteLine($"Количество согласных:  {tl.soGlas(d)}");
+            if (tl.WhatMore(d))
+            {
+                Console.WriteLine($"гласных больше");
+            }
+            else
+            {
+                Console.WriteLine($"согласных больше:");
+            }
+
+        }
+    }
+}
